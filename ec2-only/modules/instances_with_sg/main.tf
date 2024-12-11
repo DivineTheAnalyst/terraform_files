09722c3f46669e0c880538a5656c1fddca835859
Kavetec <info.kavetec@gmail.com>
2024-12-11 20:44:03 +0100

    Update main.tf

--- ec2-only/modules/instances_with_sg/main.tf
+++ ec2-only/modules/instances_with_sg/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -28,15 +28,11 @@ resource "aws_instance" "web_instance" {
   ami           = "ami-0866a3c8686eaeeba" # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"
 
+  key_name = "the-main" # Specifies the key pair to use for the instance
+
   security_groups = [aws_security_group.web_sg.name]
 
   tags = {
     Name = "WebServer-${count.index + 1}"
   }
 }
-
-
-
-
-
-
